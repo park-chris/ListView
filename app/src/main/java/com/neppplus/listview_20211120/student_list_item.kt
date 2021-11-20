dc42c491fb5b827854ce0c3f29bae96355ad0194
박수정 <zxzx4342@naver.com>
2021-11-20 12:12:56 +0900

    listview 화면

---
+++ app/src/main/java/com/neppplus/listview_20211120/student_list_item.kt
@@ -0,0 +1,11 @@
+package com.neppplus.listview_20211120
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class student_list_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_list_item)
+    }
+}
